e0966db4cf021a86de0a86dc9e93388a5f9ff491
newbeess <newbeessfu@gmail.com>
2016-07-14 13:52:14 +0800

    accelerate Carousel onload

--- web/js/banner.js
+++ web/js/banner.js
@@ -1,18 +1,21 @@
 var slideIndex = 0;
-carousel();
+// 页面加载完成后，再执行JS函数
+window.onload = function () {
+    carousel();
+}
 function carousel() {
- // 获取 待滚动的图片
- var x = document.getElementsByClassName("mySlides");
- // 将所有图片设置为不显示
- for ( var i = 0; i < x.length; i++) {
-  x[i].style.display = "none";
- }
- // 如果超出范围，从第一张开始
- slideIndex++;
- if (slideIndex > x.length) {slideIndex = 1}
- // 检查范围，显示图片
- if(slideIndex>=1 && slideIndex <= x.length){
-  x[slideIndex-1].style.display = "inline";
- }
- setTimeout(carousel, 2000); // Change image every 2 seconds
+    // 获取 待滚动的图片
+    var x = document.getElementsByClassName("mySlides");
+    // 将所有图片设置为不显示
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    // 检查范围
+    if (slideIndex >= x.length) {
+        slideIndex = 0;
+    }
+    // 显示图片
+    x[slideIndex].style.display = "block";
+    slideIndex++;
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
